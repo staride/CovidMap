afef9639e64f5e780c5d982e5a2d3dc05146388c
Kain <staride@naver.com>
2020-12-05 21:27:43 +0900

    vue config change

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   configureWebpack: {
-    // other webpack options to merge in
-  }
-  // dev Server Options don't belong into 'configureWebpack'
+      // other webpack options to merge in ... },
+  //devServer Options don't belong into `configureWebpack`
   devServer: {
     host: '0.0.0.0',
-    hto: true,
-    disableHostCheck: true
-  }
-}
+    hot: true,
+    disableHostCheck: true,
+  },
+};
